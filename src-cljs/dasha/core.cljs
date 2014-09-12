117a58271aace1a2f4a80dae631f3e6a73585d46
Aleksandr Sher <aleksandrsher@gmail.com>
2014-09-12 17:00:36 +0400

    Test web socket connection works

--- src-cljs/dasha/core.cljs
+++ src-cljs/dasha/core.cljs
@@ -1,7 +1,41 @@
-(ns dasha.core)
+(ns dasha.core
+  (:require-macros
+   [cljs.core.async.macros :as asyncm :refer (go go-loop)])
+  (:require
+   ;; <other stuff>
+   [cljs.core.async :as async :refer (<! >! put! chan)]
+   [taoensso.sente  :as sente :refer (cb-success?)]))
 
 (defn log
   [data]
   (.log js/console (str data)))
 
-(log "test")
+(defn route-receive
+  [[event data]]
+  (log (str "received " event " with data: " data))
+    )
+
+(defn route-event
+  [[event-id data :as all]]
+  (case event-id
+    :chsk/state (log (str "first open: " (:first-open? data)))
+    :chsk/recv (route-receive data)
+    (log (str "undefined event: " all))))
+
+(let [{:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket! "/chsk" ; Note the same path as before
+       {:type :auto ; e/o #{:auto :ajax :ws}
+       })]
+  (def chsk       chsk)
+  (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
+  (def chsk-send! send-fn) ; ChannelSocket's send API fn
+  (def chsk-state state)   ; Watchable, read-only atom
+
+  (go
+     (while true
+       (route-event (:event (<! ch-recv)))))
+
+  )
+
+
+
