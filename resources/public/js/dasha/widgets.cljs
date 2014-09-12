ef1e062edde881050d1d9ccfec5371772827e46f
Aleksandr Sher <aleksandrsher@gmail.com>
2014-09-12 23:48:51 +0400

    remove unused files

--- resources/public/js/dasha/widgets.cljs
+++
@@ -1,6 +0,0 @@
-(ns dasha.widgets
-  (:require [reagent.core :as reagent :refer [atom]]
-            [dasha.widgets.templates.value :as v]))
-
-
-(def widget (v/create {:value 123 :icon :i.fa.fa-random.fa-3x}))
